b60ae533281f3d7686797b10066fa3f8fb08c4b4
Henri Sota <sotahenry@gmail.com>
2022-04-19 23:54:12 +0200

    Add driver code for working with .bmp images

---
+++ image_read/main.c
@@ -0,0 +1,35 @@
+#include "imageio.h"
+
+int main(int argc, char *argv[]) {
+    char name[80];
+    long length;
+    long width;
+
+    short **image;
+    struct bmpfileheader file_header;
+    struct bitmapheader bm_header;
+
+    if (argc < 2) {
+        printf("Not enough parameters");
+    }
+
+    strcpy(name, argv[1]);
+
+    get_image_size(name, &length, &width);
+
+    image = allocate_image_array(length, width);
+
+    read_image_array(name, image);
+
+    print_image_array(image, length, width);
+
+    read_bmp_file_header(name, &file_header);
+
+    print_bmp_file_header(&file_header);
+
+    read_bm_header(name, &bm_header);
+
+    print_bm_header(&bm_header);
+
+    return 0;
+}
