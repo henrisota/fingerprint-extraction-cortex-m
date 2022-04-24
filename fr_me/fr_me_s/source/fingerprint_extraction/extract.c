26fc659739083d4578f1806e186195e74ed7bfca
Henri Sota <sotahenry@gmail.com>
2022-04-24 20:35:44 +0200

    Add temporary debug code for copying memory

--- fr_me/fr_me_s/source/fingerprint_extraction/extract.c
+++ fr_me/fr_me_s/source/fingerprint_extraction/extract.c
@@ -9,6 +9,12 @@
 #include "extract.h"
 #include "../secret/secret.h"
 
+#include "../data.h"
+
+#include <stdio.h>
+
+struct fingerprint_image_t finger;
+
 unsigned int LoadFingerprintImage(unsigned char fingerprint[][10], unsigned int length, unsigned int width, char *key) {
     PRINTF("Loading fingerprint image in the secure world...\n\n");
 
@@ -19,6 +25,51 @@ unsigned int LoadFingerprintImage(unsigned char fingerprint[][10], unsigned int 
         PRINTF("Provided key matches the stored key\n\n");
 
         PRINTF("Storing fingerprint image data on the secure world...\n");
+
+        int copy_size = length * width;
+        int left_page_size = 32766;
+        int max_copy_round_size = 100 * width;
+        int data_pos = 0;
+
+        // while (1) {
+        //     if (copy_size == 0)
+        //         break;
+
+        //     PRINTF("%d | %d | %d | %d\n", copy_size, left_page_size, max_copy_round_size, data_pos);
+
+        //     if (copy_size < left_page_size) {
+        //         left_page_size = copy_size;
+        //     }
+
+        //     if (max_copy_round_size < left_page_size) {
+        //         left_page_size -= max_copy_round_size;
+        //         memcpy(&(finger.data) + data_pos, fingerprint + data_pos, max_copy_round_size);
+
+        //         data_pos += max_copy_round_size;
+        //         copy_size -= max_copy_round_size;
+        //     } else {
+        //         memcpy(&(finger.data) + data_pos, fingerprint + data_pos, left_page_size);
+
+        //         data_pos += left_page_size;
+        //         copy_size -= left_page_size;
+
+        //         left_page_size = 32768;
+        //     }
+        // }
+        memcpy(&(finger.data) + data_pos, fingerprint + data_pos, 30000);
+
+        int i;
+        int j;
+
+        for (i = 0; i < 1; ++i) {
+            for (j = 0; j < width; ++j) {
+                PRINTF("%d ", finger.data[i][j]);
+            }
+
+            PRINTF("\n");
+        }
+
+        PRINTF("data: %d\n", finger.data[0][0]);
 
         return 1;
     }
