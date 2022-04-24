703beb76b1f774fa13722e6f3469b0f502a83f82
Henri Sota <sotahenry@gmail.com>
2022-04-24 20:34:02 +0200

    Add definition of fingerprint struct

---
+++ fr_me/fr_me_s/source/data.h
@@ -0,0 +1,20 @@
+/*
+ * data.h
+ *
+ *  Created on: Apr 24, 2022
+ *      Author: dante
+ */
+
+#ifndef _DATA_H_
+#define _DATA_H_
+
+#define LENGTH 300
+#define WIDTH  300
+
+struct fingerprint_image_t {
+    unsigned int length;
+    unsigned int width;
+    unsigned char data[100][WIDTH];
+};
+
+#endif /* DATA_H_ */
