42b9f6631b8ecca800dbe0d9fcba4c1912e445cf
Henri Sota <sotahenry@gmail.com>
2022-04-27 23:39:16 +0200

    Clean extract from preprocessing methods

--- fr_me_local/extract.h
+++ fr_me_local/extract.h
@@ -1,7 +1,4 @@
 #ifndef _EXTRACT_H
 #define _EXTRACT_H
 
-unsigned char **read_grayscale_binary_file(const char *filename, long *length, long *width);
-int print_image_array(unsigned char **image_array, long length, long width);
-
 #endif
