21f243ba96c1813bf52d4ba207b30415e2fcf6e4
Henri Sota <sotahenry@gmail.com>
2022-04-30 01:05:51 +0200

    Add extraction method declaration

--- fr_me_local/extract.h
+++ fr_me_local/extract.h
@@ -1,4 +1,6 @@
 #ifndef _EXTRACT_H
 #define _EXTRACT_H
 
+int extract(unsigned char **image, long length, long width, unsigned char **segmentation_array, int segmentation_image_array_size);
+
 #endif
