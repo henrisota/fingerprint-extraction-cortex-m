052b734412190f31efc26cf4d8648ee95b1caf3f
Henri Sota <sotahenry@gmail.com>
2022-05-01 00:50:29 +0200

    Update definition of extraction

--- fr_me_local/extract.h
+++ fr_me_local/extract.h
@@ -1,6 +1,6 @@
 #ifndef _EXTRACT_H
 #define _EXTRACT_H
 
-int extract(unsigned char **image, long length, long width, unsigned char **segmentation_array, int segmentation_image_array_size);
+int extract(unsigned char **image, long length, long width, unsigned char **segmentation_array, int segmentation_array_size);
 
 #endif
