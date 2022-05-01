76dc078e876a18df593966b485ba0de7449f4228
Henri Sota <sotahenry@gmail.com>
2022-05-01 16:55:47 +0200

    Add contrast enhancement method and cleanup

--- fr_me_local/enhance.c
+++ fr_me_local/enhance.c
@@ -10,8 +10,6 @@ int normalize(unsigned char **image, long length, long width, float image_mean, 
         printf("Starting image normalization.\n");
     }
 
-    printf("Image mean: %f\n", image_mean);
-    printf("Image variance: %f\n", image_variance);
     int i;
     int j;
 
@@ -37,6 +35,56 @@ int normalize(unsigned char **image, long length, long width, float image_mean, 
     return 1;
 }
 
+int increase_contrast(unsigned char **image, long length, long width) {
+    int i;
+    int j;
+    int k;
+    int l;
+
+    char half_block = CONTRAST_BLOCK_SIZE / 2;
+
+    int pixel_pos;
+    int intensity;
+    float mean;
+    int difference = 0;
+    int contrasted_intensity = 0;
+
+    // Iterate over the inner pixels of the image, avoiding the outer pixels
+    for (i = half_block; i < length - half_block; ++i) {
+        for (j = half_block; j < width - half_block; ++j) {
+            mean = 0;
+
+            for (k = -half_block; k <= half_block; ++k) {
+                for (l = -half_block; l <= half_block; ++l) {
+                    pixel_pos = width * (i + k) + (j + l);
+                    intensity = image[pixel_pos / MAX_IMAGE_WIDTH][pixel_pos % MAX_IMAGE_WIDTH];
+
+                    mean += intensity;
+                }
+            }
+
+            mean /= CONTRAST_BLOCK_SIZE * CONTRAST_BLOCK_SIZE;
+
+            pixel_pos = width * i + j;
+            intensity = image[pixel_pos / MAX_IMAGE_WIDTH][pixel_pos % MAX_IMAGE_WIDTH];
+
+            difference = (intensity - (int) mean);
+
+            contrasted_intensity = mean + CONTRAST_CONSTANT * difference;
+
+            if (contrasted_intensity < 0) {
+                contrasted_intensity = 0;
+            } else if (contrasted_intensity > 255) {
+                contrasted_intensity = 255;
+            }
+
+            image[pixel_pos / MAX_IMAGE_WIDTH][pixel_pos % MAX_IMAGE_WIDTH] = (unsigned char) contrasted_intensity;
+        }
+    }
+
+    return 1;
+}
+
 int enhance(unsigned char **image, long length, long width) {
     if (VERBOSE) {
         printf("Starting image enhancement.\n");
@@ -46,6 +94,8 @@ int enhance(unsigned char **image, long length, long width) {
     float image_variance = calculate_image_variance(image, length, width, image_mean);
 
     normalize(image, length, width, image_mean, image_variance);
+
+    increase_contrast(image, length, width);
 
     if (VERBOSE) {
         printf("Finished image enhancement.\n");
