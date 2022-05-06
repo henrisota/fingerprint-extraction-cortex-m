a547abc27628eefc3e6945859970d96380978e74
Henri Sota <sotahenry@gmail.com>
2022-05-06 14:14:11 +0200

    Update logging

--- fr_me/fr_me_s/source/fingerprint_extraction/extract.c
+++ fr_me/fr_me_s/source/fingerprint_extraction/extract.c
@@ -9,60 +9,73 @@
 
 #include "../config.h"
 
-#include "extract.h"
-#include "../secret/secret.h"
 #include "../data.h"
+#include "../secret/secret.h"
+#include "extract.h"
+#include "preprocess.h"
 
+// Keep the grayscale image
 struct fingerprint_image_t finger;
+// Keep the segmentation of the blocks of the image
+unsigned char segmentation_array[SEGMENTATION_ARRAY_SIZE] = { 0 };
+int segmentation_array_size;
+// Keep the orientation of the blocks of the image
+unsigned char orientation_array[ORIENTATION_ARRAY_SIZE] = { 0 };
+int orientation_array_size;
 
 unsigned int LoadFingerprintImage(unsigned char fingerprint[][10], unsigned int length, unsigned int width, char *key) {
-	if (SHOW_DEBUG) {
-	    PRINTF("Loading fingerprint image in the secure world...\n\n");
+    if (VERBOSE) {
+        PRINTF("Loading fingerprint image in the secure world...\r\n\r\n");
 
-	    PRINTF("Checking the provided key against the stored key...\n");
-	}
+        PRINTF("Checking the provided key against the stored key...\r\n");
+    }
 
     // Verify if the provided key is the correct on to continue loading the fingerprint
     if (VerifyKey(key)) {
-    	if (SHOW_DEBUG) {
-            PRINTF("Provided key matches the stored key\n\n");
+        if (VERBOSE) {
+            PRINTF("Provided key matches the stored key\r\n\r\n");
 
-            PRINTF("Storing fingerprint image data on the secure world...\n");
-    	}
+            PRINTF("Storing fingerprint image data on the secure world...\r\n");
+        }
 
+        finger.length = length;
+        finger.width = width;
         memcpy(&(finger.data), fingerprint, length * width);
 
-        if (SHOW_DEBUG) {
+        if (VERBOSE) {
             int i;
             int j;
 
-			for (j = 0; j < width; ++j) {
-				PRINTF("%d ", finger.data[0][j]);
-			}
-			PRINTF("\n");
+            for (j = 0; j < width; ++j) {
+                PRINTF("%d ", finger.data[j / MAX_IMAGE_WIDTH][j % MAX_IMAGE_WIDTH]);
+            }
+            PRINTF("\n");
 
-			PRINTF("...\n");
+            PRINTF("...\n");
 
-			for (j = 0; j < width; ++j) {
-				PRINTF("%d ", finger.data[1][j]);
-			}
-			PRINTF("\n");
+            for (j = 0; j < width; ++j) {
+                PRINTF("%d ", finger.data[(length * width + j) / MAX_IMAGE_WIDTH][(length * width + j % MAX_IMAGE_WIDTH)]);
+            }
+            PRINTF("\n");
         }
 
         return 1;
     }
 
-    PRINTF("Provided key does not match the stored key\n");
-    PRINTF("Stopping fingerprint loading procedure\n");
+    PRINTF("Provided key does not match the stored key\r\n");
+    PRINTF("Stopping fingerprint loading procedure\r\n");
 
     return 0;
 }
 
 unsigned int ExtractMinutiae(unsigned int length, unsigned int width) {
-	if (SHOW_DEBUG) {
-	    PRINTF("Extracting the minutiae from the fingerprint image in the secure world...\n\n");
-	}
+    if (VERBOSE) {
+        PRINTF("Starting extraction of minutiae from the fingerprint image in the secure world.\r\n\r\n");
+    }
 
+    Preprocess(finger.data, finger.length, finger.width, segmentation_array, &segmentation_array_size);
 
+    if (VERBOSE) {
+        PRINTF("Finished extraction of minutiae from the fingerprint image in the secure world\r\n");
+    }
 }
-
--- fr_me/fr_me_s/source/fingerprint_extraction/extract.h
+++ fr_me/fr_me_s/source/fingerprint_extraction/extract.h
@@ -5,10 +5,10 @@
  *      Author: dante
  */
 
-#ifndef _EXTRACT_H_
-#define _EXTRACT_H_
+#ifndef _FINGERPRINT_EXTRACTION_EXTRACT_H_
+#define _FINGERPRINT_EXTRACTION_EXTRACT_H_
 
 unsigned int LoadFingerprintImage(unsigned char fingerprint[][10], unsigned int length, unsigned int width, char *key);
 unsigned int ExtractMinutiae(unsigned int length, unsigned int width);
 
-#endif /* _EXTRACT_H_ */
+#endif /* _FINGERPRINT_EXTRACTION_EXTRACT_H_ */
