c7304a3f836d7c311e9eb8a8d2d95f1a21f23240
Henri Sota <sotahenry@gmail.com>
2022-05-06 16:27:54 +0200

    Log segmentation to complete preprocess flow porting

--- fr_me/fr_me_s/source/fingerprint_extraction/preprocess.c
+++ fr_me/fr_me_s/source/fingerprint_extraction/preprocess.c
@@ -7,24 +7,28 @@
 
 #include "fsl_debug_console.h"
 
-#include "../config.h"
-#include "preprocess.h"
 #include "../data.h"
+#include "../config.h"
+#include "enhance.h"
+#include "preprocess.h"
+#include "segment.h"
 
 int Preprocess(unsigned char image[MAX_IMAGE_LENGTH][MAX_IMAGE_WIDTH], unsigned int length, unsigned int width, unsigned char *segmentation_array, int *segmentation_array_size) {
     if (VERBOSE) {
         PRINTF("Started preprocessing.\r\n");
     }
 
-    int i;
-    int j;
+    *segmentation_array_size = 0;
 
+    Enhance(image, length, width);
 
-
+    Segment(image, length, width, segmentation_array, segmentation_array_size);
 
     if (VERBOSE) {
         PRINTF("Finished preprocessing.\r\n");
     }
+
+    PrintSegmentationArray(segmentation_array, *segmentation_array_size, length, width);
 
     return 1;
 }
